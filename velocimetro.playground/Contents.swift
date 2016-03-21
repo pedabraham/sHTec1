2fee4b9c822597592b21572087fe03bfbca65b7c
pedabraham <pedabraham@gmail.com>
2016-03-20 23:56:10 -0600

    Added files via upload

---
+++ velocimetro.playground/Contents.swift
@@ -0,0 +1,61 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+
+enum velocidades:Int {
+    case Apagado =  0, VelocidadBaja = 20, VelocidadMedia = 50, VelocidadALta = 120
+    
+   
+    
+    init( velocidadInicial : velocidades ){
+        self = velocidadInicial
+    }
+}
+
+
+
+
+class Auto {
+    
+    var velocidad = velocidades()
+    
+    init(velocidad:velocidades ){
+        self.velocidad = .Apagado
+    }
+    
+    func CambioDeVelocidad () -> (actual : Int, velocidadEnCadena: String){
+        var velo: String
+        
+        switch velocidad {
+        case .Apagado:
+            velocidad = .VelocidadBaja
+            velo = "VelocidadBaja"
+        case .VelocidadBaja:
+            velocidad = .VelocidadMedia
+            velo = "VelocidadMedia"
+        case .VelocidadMedia:
+            velocidad = .VelocidadALta
+            velo = "VelocidadAlta"
+        case .VelocidadALta:
+            velocidad = .VelocidadMedia
+            velo = "VelocidadMedia"
+        
+        }
+        
+       
+        let resultado = (velocidad, velo)
+        return resultado
+        
+    }
+    
+}
+
+
+var auto = Auto.self
+
+
+for n in 1...20 {
+    print(auto.CambioDeVelocidad())
+}
